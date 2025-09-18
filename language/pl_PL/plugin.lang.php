479b37281748ff258bbbc0bcc22a3bb85521cfe9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-18 13:00:04 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['CAS Simple Auth'] = 'Uwierzytelnij za pomocą:';
