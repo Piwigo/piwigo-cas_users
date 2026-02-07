4268a8c37860f7a3c7c0664f45bcf3c9fe9aa398
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-07 19:16:01 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['CAS Simple Auth'] = 'Authentifizieren mit:';
+$lang['Or sign in with'] = 'Oder melden Sie sich an mit';
