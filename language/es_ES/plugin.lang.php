f49fbc9c1c3c73e181aeb783e6fc6fcb1be37c6e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-07 19:15:46 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['CAS Simple Auth'] = 'Autentíquese con:';
+$lang['Or sign in with'] = 'O inicie sesión con';
