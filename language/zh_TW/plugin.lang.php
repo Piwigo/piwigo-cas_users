99d9cdc0a1b9b1d38dea3d8c458eba7c5e31ed15
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-10 22:33:54 +0100

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['CAS Simple Auth'] = '使用以下方式認證：';
+$lang['Or sign in with'] = '或使用此方式登入';
